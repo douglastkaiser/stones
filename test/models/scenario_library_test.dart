9dba771e8d43520b5ed30661696b5f427f72804b
Douglas Kaiser <51721139+douglastkaiser@users.noreply.github.com>
2026-04-20 16:17:08 -0500

    Import AI move types in scenario library test

--- test/models/scenario_library_test.dart
+++ test/models/scenario_library_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:stones/models/models.dart';
+import 'package:stones/services/ai/ai.dart';
 
 void main() {
   group('tutorialAndPuzzleLibrary', () {
